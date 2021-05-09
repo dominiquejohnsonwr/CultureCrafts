095855a6c074ba957cdb9a435c261d3b95d46d1e
dominiquejohnsonwr <81428925+dominiquejohnsonwr@users.noreply.github.com>
2021-05-09 01:40:37 -0700

    added user model and migrated schema

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
-require "active_support/core_ext/integer/time"
+require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
@@ -49,7 +51,6 @@ Rails.application.configure do
 
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
-
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
