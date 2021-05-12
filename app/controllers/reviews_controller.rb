d77e648db1e94f3745cc7f278f5247f48101e996
dominiquejohnsonwr <81428925+dominiquejohnsonwr@users.noreply.github.com>
2021-05-12 10:18:15 -0700

    added reviews controller on back end

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,12 @@
-class ReviewsController < ApplicationController
-  before_action :set_review, only: [:show, :update, :destroy]
+class ReviewsController < ApiController
+  skip_before_action :authenticate_user!, only: %i[index show]
+  before_action :set_review, only: %i[show update destroy]
 
   # GET /reviews
   def index
-    @reviews = Review.all
+    @reviews = Product.find(params[:product_id]).reviews
 
-    render json: @reviews
+    render json: @reviews, include: [:product]
   end
 
   # GET /reviews/1
@@ -15,7 +16,8 @@ class ReviewsController < ApplicationController
 
   # POST /reviews
   def create
-    @review = Review.new(review_params)
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.build(review_params)
 
     if @review.save
       render json: @review, status: :created, location: @review
@@ -39,13 +41,14 @@ class ReviewsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_review
-      @review = Review.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def review_params
-      params.require(:review).permit(:content, :product_id, :user_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_review
+    @review = Review.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def review_params
+    params.require(:review).permit(:content, :product_id, :user_id)
+  end
 end
