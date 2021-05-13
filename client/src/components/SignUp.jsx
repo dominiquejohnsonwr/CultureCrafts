808c250d4424a122bd565ed30649072ae4ce0165
dominiquejohnsonwr <81428925+dominiquejohnsonwr@users.noreply.github.com>
2021-05-13 09:04:18 -0700

    completed signup form css

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { loginUser, registerUser } from "../services/auth";
-import {useHistory} from "react-router-dom"
+import { useHistory } from "react-router-dom"
+import './SignUp.css'
 
 function SignUp(props) {
   let history = useHistory()
@@ -23,21 +24,23 @@ function SignUp(props) {
     console.log(res);
   };
   return (
-    <div>
+    <div className='signup-title'>
       <h1>Sign Up</h1>
-      <form onChange={handleChange} onSubmit={handleSubmit}>
-        <label>Username:</label>
-        <input name="name" type="text"/>
-        <label>Email:</label>
-        <input name="email" type="email" />
-        <label>Profile Image Url:</label>
-        <input name="profile_img" type="text" placeholder="paste your image url here..." />
-        <label>Password:</label>
-        <input name="password" type="password" />
-        <label>Password Confirmation:</label>
-        <input name="password_confirmation" type="password" />
-        <button type="submit">Sign Up</button>
-      </form>
+      <div className='signup-container'>
+        <form className='signup-form' onChange={handleChange} onSubmit={handleSubmit}>
+          <label>Username:</label>
+          <input name="name" type="text"/>
+          <label>Email:</label>
+          <input name="email" type="email" />
+          <label>Profile Image Url:</label>
+          <input name="profile_img" type="text" />
+          <label>Password:</label>
+          <input name="password" type="password" />
+          <label>Password Confirmation:</label>
+          <input name="password_confirmation" type="password" />
+          <button className='login-submit' type="submit">Sign Up</button>
+        </form>
+      </div>
     </div>
   );
 }
